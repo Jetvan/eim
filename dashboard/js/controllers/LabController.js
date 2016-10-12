4ba370d7dfd0cef688f7b2e80a91436ab8c67cd3
hb-bobo <751968840@qq.com>
2016-10-12 10:15:02 +0800

    1·

--- dashboard/js/controllers/LabController.js
+++ dashboard/js/controllers/LabController.js
@@ -67,12 +67,12 @@ $scope.statusInfo = {
         },
 };
 $scope.status = {
-    "0":1,
-    "1":1,
-    "2":1,
-    "3":1,
-    "4":1,
-    "5":1,
+    "0":0,
+    "1":0,
+    "2":0,
+    "3":0,
+    "4":0,
+    "5":0,
 };
 
     $scope.$on('$viewContentLoaded', function() {  
